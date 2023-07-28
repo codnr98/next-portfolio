b7f69410af6255411020f43cfa2e41072ead505a
codnr <wnd75913@gmail.com>
2023-07-28 11:37:47 +0900

    fix: eslint airbnb/hooks 제거, function component definition 설정 수정

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren } from 'react'
 
-export const Layout: FC<PropsWithChildren> = ({ children }) => {
+const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <h1>레이아웃</h1>
@@ -8,3 +8,5 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
     </>
   )
 }
+
+export default Layout
