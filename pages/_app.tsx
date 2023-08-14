804cee5d48c058f40e594fdcf89173aa8471199f
codnr <wnd75913@gmail.com>
2023-08-14 16:55:22 +0900

    feat: DarkModeToggleButton 컴포넌트 기능 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default App
