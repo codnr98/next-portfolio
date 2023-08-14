0e36bb290e9cd153c0ba92cf5f301f9a151459d9
codnr <wnd75913@gmail.com>
2023-08-14 16:28:04 +0900

    feat: Link 작업, projects 페이지에 Head 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         <title>codnr 포트폴리오</title>
         <meta name="description" content="codnr의 포트폴리오 입니다." />
       </Head>
-      <section className="flex min-h-screen flex-col items-center justify-center   text-gray-600 body-font">
+      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,13 @@
 import Layout from '@/components/layout'
+import Head from 'next/head'
 
 const Projects = () => {
   return (
     <Layout>
+      <Head>
+        <title>codnr 포트폴리오</title>
+        <meta name="description" content="codnr의 포트폴리오 입니다." />
+      </Head>
       <h1>프로젝트</h1>
     </Layout>
   )
