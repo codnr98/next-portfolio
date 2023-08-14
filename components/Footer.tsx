d16264920aa1fbb7d81a15c9907eaa2efc98fe3f
codnr <wnd75913@gmail.com>
2023-08-14 16:32:05 +0900

    feat: footer Link 작업

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
+
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <Link
+          href="/"
+          className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+        >
           <span className="ml-3 text-xl">이채욱의 포트폴리오</span>
-        </a>
+        </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2020 Tailblocks —
           <a
