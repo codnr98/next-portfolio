04f6dd2b61aaef0939280707f428fa21de7dc706
codnr <wnd75913@gmail.com>
2023-08-18 17:24:23 +0900

    feat: Repo type 선언

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,10 +2,7 @@ import Layout from '@/components/layout'
 import Head from 'next/head'
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import loadProjects from '@/apis/load-projects'
-
-type Repo = {
-  results: []
-}
+import { Repo } from '@/types/project-data'
 
 export const getStaticProps: GetStaticProps<{
   projects: Repo
@@ -24,8 +21,18 @@ const Projects = ({
         <title>codnr 포트폴리오</title>
         <meta name="description" content="codnr의 포트폴리오 입니다." />
       </Head>
-      <h1>프로젝트</h1>
-      <p>총 프로젝트 : {projects.results.length}</p>
+      <h1>총 프로젝트 : {projects.results.length}</h1>
+      {projects.results.map((projectsDates) => (
+        <div key={projectsDates.id}>
+          <h1>{projectsDates.properties.이름.title[0]?.text.content}</h1>
+          <p>
+            {projectsDates.properties.Description.rich_text[0]?.text.content}
+          </p>
+          <p>{projectsDates.properties.Github.url}</p>
+          <p>{projectsDates.properties.workPeriod.date?.start}</p>
+          <p>{projectsDates.properties.workPeriod.date?.end}</p>
+        </div>
+      ))}
     </Layout>
   )
 }
---
+++ types/project-data.ts
@@ -0,0 +1,63 @@
+export type Repo = {
+  results: Results[]
+}
+
+type Cover = {
+  type: string
+  external: {
+    url: string
+  }
+}
+
+type Results = {
+  id: string
+  created_time: string
+  last_edited_time: string
+  cover: Cover
+  properties: Properties
+  url: string
+}
+
+type Properties = {
+  태그: MultiselectProperty
+  workPeriod: WorkPeriod
+  Description: Description
+  Github: UrlProperty
+  이름: TitleProperty
+}
+
+type MultiselectProperty = {
+  multi_select: MultiselectOption[]
+}
+type MultiselectOption = {
+  id: string
+  name: string
+  color: string
+}
+
+type WorkPeriod = {
+  date: {
+    start: string
+    end: string
+  }
+}
+
+type Description = {
+  rich_text: RichText[]
+}
+
+type RichText = {
+  text: {
+    content: string
+  }
+}
+
+type UrlProperty = {
+  url: string
+}
+
+type TitleProperty = {
+  id: string
+  type: string
+  title: RichText[]
+}
