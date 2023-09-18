92384499fd2d9a51abfe6fa2fcedc6ff51131354
codnr <wnd75913@gmail.com>
2023-09-18 13:39:20 +0900

    doc: Hero 컨텐츠 수정

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,18 +6,13 @@ const Hero = () => {
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          안녕하세요. 프론트엔드 개발자 이채욱입니다.
+          이채욱 FrontEnd Developer
           <br className="hidden lg:inline-block" />
         </h1>
         <p className="mb-8 leading-relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam,
-          officia laboriosam? Hic, magnam deleniti. Ea odit repudiandae qui
-          impedit culpa. Quaerat magnam corporis a odio voluptatem consectetur
-          cum asperiores exercitationem! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Quidem aut provident veniam fugit accusantium eius,
-          quia illo repellat, quaerat eaque sunt vitae architecto facilis cum
-          explicabo corrupti facere eveniet doloremque.
+          안녕하세요, TypeScript, ReactJS 기반의 프론트엔드 개발자 입니다.
         </p>
+        <p className="mb-8 leading-relaxed" />
         <div className="flex justify-center">
           <Link href="projects" className="btn-project">
             프로젝트 보러가기
