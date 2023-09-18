fcaf262b89ff42ee9d7b8f237ccb796380f15e8f
codnr <wnd75913@gmail.com>
2023-09-18 18:00:13 +0900

    feat: footer 내용수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,14 +12,6 @@ const Footer = () => {
         </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2023. codnr Co. all rights reserved.
-          <a
-            href="https://twitter.com/knyttneve"
-            className="text-gray-600 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            @knyttneve
-          </a>
         </p>
       </div>
     </footer>
