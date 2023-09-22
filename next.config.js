f27960708d4d0c7a37c640e898f1fe2bc6777723
codnr <wnd75913@gmail.com>
2023-09-22 15:40:01 +0900

    fix: next.config의 images domain 수정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.notion.so', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
+    domains: [
+      'www.notion.so',
+      'prod-files-secure.s3.us-west-2.amazonaws.com',
+      'prod-files-secure.s3.us-west-2.amazonaws.com/37916ca4-6cf3-44b7-8a6a-ed4f43e55de7',
+    ],
   },
 }
 
