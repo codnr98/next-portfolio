b4d53ce08a26bc0aa1b2ef9aafecb3f447c3a714
codnr <wnd75913@gmail.com>
2023-09-26 15:08:22 +0900

    fix: image 도메인 변경

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       'www.notion.so',
       'prod-files-secure.s3.us-west-2.amazonaws.com',
-      'prod-files-secure.s3.us-west-2.amazonaws.com/37916ca4-6cf3-44b7-8a6a-ed4f43e55de7',
+      's3.us-west-2.amazonaws.com',
     ],
   },
 }
