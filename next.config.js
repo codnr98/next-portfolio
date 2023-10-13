0a3efa613ba1e330d88b4a562fa3ada52a1a10f0
codnr <wnd75913@gmail.com>
2023-10-13 10:26:27 +0900

    fix: next.config의 이미지 도메인 13버전 설정으로 수정

--- next.config.js
+++ next.config.js
@@ -2,10 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'www.notion.so',
-      'prod-files-secure.s3.us-west-2.amazonaws.com',
-      's3.us-west-2.amazonaws.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
