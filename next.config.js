77008f90d667f4464efe092022816bb2b4b232a0
codnr <wnd75913@gmail.com>
2023-11-11 14:52:50 +0900

    fix: 이미지 도메인 수정

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/37916ca4-6cf3-44b7-8a6a-ed4f43e55de7/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/*',
+      },
     ],
   },
 }
